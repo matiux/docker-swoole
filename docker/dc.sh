1919f97daa25be405a1a70d505541a8808712a35
Matteo <m.galacci@gmail.com>
2020-11-03 22:24:56 +0100

    feat: aggiunge immagini per swoole 4.5.6 su php 7.4.11

--- docker/dc.sh
+++ docker/dc.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-PROJECT_NAME=$(basename $(pwd) | tr '[:upper:]' '[:lower:]')
+WORKDIR=/var/www/app
+PROJECT_NAME=$(basename "$(pwd)" | tr '[:upper:]' '[:lower:]')
 COMPOSE_OVERRIDE=
-PHP_CONTAINER=php
-PHP_USER=utente
+PHP_CONTAINER=php_swoole_csv_handler
 
 if [[ -f "./docker/docker-compose.override.yml" ]]; then
   COMPOSE_OVERRIDE="--file ./docker/docker-compose.override.yml"
@@ -16,16 +16,48 @@ DC_BASE_COMMAND="docker-compose
 
 DC_RUN="${DC_BASE_COMMAND}
     run
-    --service-ports
     --rm
-    -u ${PHP_USER}
+    -u utente
+    -v ${PWD}:${WORKDIR}
+    -w ${WORKDIR}
     ${PHP_CONTAINER}"
+
+#-T
+DC_EXEC="${DC_BASE_COMMAND}
+  exec
+  -u utente
+  -w ${WORKDIR}
+  ${PHP_CONTAINER}"
 
 if [[ "$1" == "composer" ]]; then
 
   shift 1
   ${DC_RUN} \
     composer "$@"
+
+elif [[ "$1" == "php-cs-fixer-fix" ]]; then
+
+  shift 1
+  ${DC_EXEC} \
+    vendor/bin/php-cs-fixer fix --config=.php_cs.dist "$@"
+
+elif [[ "$1" == "php-cs-fixer" ]]; then
+
+  shift 1
+  ${DC_EXEC} \
+    vendor/bin/php-cs-fixer "$@"
+
+elif [[ "$1" == "psalm" ]]; then
+
+  shift 1
+  ${DC_EXEC} \
+    vendor/bin/psalm "$@"
+
+elif [[ "$1" == "phpunit" ]]; then
+
+  shift 1
+  ${DC_EXEC} \
+    vendor/bin/phpunit "$@"
 
 elif [[ "$1" == "up" ]]; then
 
@@ -38,19 +70,15 @@ elif [[ "$1" == "build" ]] && [[ "$2" == "php" ]]; then
   ${DC_BASE_COMMAND} \
     build ${PHP_CONTAINER}
 
-elif [[ "$1" == "enter-root" ]]; then
-
-  ${DC_BASE_COMMAND} \
-    exec \
-    -u root \
-    ${PHP_CONTAINER} /bin/bash
-
 elif [[ "$1" == "enter" ]]; then
 
-  ${DC_BASE_COMMAND} \
-    exec \
-    -u ${PHP_USER} \
-    ${PHP_CONTAINER} /bin/bash
+  ${DC_EXEC} \
+     /bin/zsh
+
+elif [[ "$1" == "enter-root" ]]; then
+
+  ${DC_EXEC} \
+    /bin/zsh
 
 elif [[ "$1" == "down" ]]; then
 
@@ -70,31 +98,6 @@ elif [[ "$1" == "log" ]]; then
 
   ${DC_BASE_COMMAND} \
     logs -f
-
-elif [[ "$1" == "exec" ]]; then
-
-  shift 1
-  ${DC_BASE_COMMAND} \
-    exec \
-    -u ${PHP_USER} \
-    ${PHP_CONTAINER} \
-    "$@"
-
-elif [[ "$1" == "run" ]]; then
-
-  shift 1
-  ${DC_RUN} \
-    "$@"
-
-elif [[ "$1" == "runr" ]]; then
-
-  PHP_USER=root
-
-  shift 1
-  ${DC_RUN} \
-    "$@"
-
-  PHP_USER=utente
 
 elif [[ $# -gt 0 ]]; then
 
